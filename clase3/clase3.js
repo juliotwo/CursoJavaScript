2de28bfb0a4bde56b274648631228ee8275bff5a
juliotwo1996 <juliotwo1996@gmail.com>
2018-10-03 19:25:12 -0500

    Callbacks

--- clase3/clase3.js
+++ clase3/clase3.js
@@ -4,17 +4,23 @@ const PEOPLE_URL = 'people/:id/'
 const lukeUrl =`${API_URL}${PEOPLE_URL.replace(':id',1)}`
 const opts = {crossDomain:true}
 
-function onResponse(persona){
 
-console.log(`Hola soy ${persona.name}`)
-
-}
-function obtenerPersonaje (id){
+function obtenerPersonaje (id, callback){
     const url = `${API_URL}${PEOPLE_URL.replace(':id',id)}`
-    $.get(url,opts,onResponse)
+    $.get(url,opts,callback)
 
 }
 
 
+obtenerPersonaje(1, function (persona){
+		console.log(`Hola soy ${persona.name}`)
+		obtenerPersonaje(2, function (persona){
+			console.log(`Hola soy ${persona.name}`)
+				obtenerPersonaje(3, function (persona){
+				console.log(`Hola soy ${persona.name}`)
+		})
+	})
 
-obtenerPersonaje(2)
+})
+
+
